654418a759821b668ce8bf5e7f331fc431eda183
JosueLabres <labresjosue@gmail.com>
2021-11-13 11:35:42 -0300

    criando o robo de imagens

--- main.py
+++ main.py
@@ -1,36 +1,45 @@
 from json import encoder
 
 from requests.models import encode_multipart_formdata
+from robots.images import ImageRobot
 from robots.text import TextRobot
 from robots.userInput import UserInput
 import json
 class Start():
     def __init__(self):
-        maxSentences = 1
-        self.searchTerm = UserInput.asKAndReturnSearchTerm()
-        self.prefix = UserInput.asKAndReturnPrefix()
+        # maxSentences = 10
+        # self.searchTerm = UserInput.asKAndReturnSearchTerm()
+        # self.prefix = UserInput.asKAndReturnPrefix()
         
-        textRobot = TextRobot(self.searchTerm)
-        self.wikipediaContent = textRobot.fetchContentFromWikipedia()
-        self.sanitizeContent = textRobot.sanitizeContent()
-        sentences = textRobot.breakContentIntoSentences(self.sanitizeContent)
-        self.sentences = textRobot.watson(sentences, maxSentences)
-    
-    def get_content(self):
-        content =  {
-            "search_term": self.searchTerm,
-            "prefix": self.prefix,
-            "wikipedia_content": self.wikipediaContent,
-            "sanitizeContent": self.sanitizeContent,
-            "sentences": self.sentences
-        }
+        # textRobot = TextRobot(self.searchTerm)
+        # self.wikipediaContent = textRobot.fetchContentFromWikipedia()
+        # self.sanitizeContent = textRobot.sanitizeContent()
+        # sentences = textRobot.breakContentIntoSentences(self.sanitizeContent)
+        # self.sentences = textRobot.watson(sentences, maxSentences)
+        
+        # self.content =  {
+        #     "search_term": self.searchTerm,
+        #     "prefix": self.prefix,
+        #     "wikipedia_content": self.wikipediaContent,
+        #     "sanitizeContent": self.sanitizeContent,
+        #     "sentences": self.sentences
+        # }
+        # with open('data.json', 'w') as outfile:
+        #     json.dump(self.content, outfile )
+
+        self.content = ImageRobot().searchImageOfSentences()
         
         with open('data.json', 'w') as outfile:
-            json.dump(content, outfile )
+            json.dump(self.content, outfile )
+        
+    def get_content(self):
+       
+        
+        
             
-        return content
+        return self.content
         
 
 
 
-print(Start().get_content())
+Start()
